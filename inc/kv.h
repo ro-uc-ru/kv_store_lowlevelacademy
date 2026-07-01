f7df2327000a0db2f98372fa4e680333f1dd0a9c
Roger <rogiruceda@gmail.com>
2026-07-01 10:24:05 +0000

    kv_init

---
+++ inc/kv.h
@@ -0,0 +1,20 @@
+# ifndef KV_H
+# define KV_H
+
+# include <stdlib.h>
+
+typedef struct kv
+{
+  char* key;
+  char* value;
+} kv_entry_t;
+
+typedef struct {
+  size_t capacity;
+  size_t count; // number of entries
+  kv_entry_t* entries; 
+} kv_t; //kv_table
+
+
+kv_t* kv_init(size_t capacity);
+# endif
